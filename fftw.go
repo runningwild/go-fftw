bcf6cf74b944cc30bdc8fd366abe6b16c3723141
Jonathan Wills <runningwild@gmail.com>
2011-01-29 15:00:52 -0500

    Made FFTW_ESTIMATE and FFTW_MEASURE available

--- fftw.go
+++ fftw.go
@@ -29,6 +29,10 @@ type Direction int
 var Forward  Direction = C.FFTW_FORWARD
 var Backward Direction = C.FFTW_BACKWARD
 
+type Flag uint
+var Estimate Flag = C.FFTW_ESTIMATE
+var Measure  Flag = C.FFTW_MEASURE
+
 func Alloc1d(n int) []complex128 {
   return make([]complex128, n)
 }
@@ -42,19 +46,19 @@ func Alloc2d(n0,n1 int) [][]complex128 {
 }
 
 
-func PlanDft1d(in,out []complex128, dir Direction) *plan {
+func PlanDft1d(in,out []complex128, dir Direction, flag Flag) *plan {
   // TODO: check that len(in) == len(out)
   fftw_in := (*C.fftw_complex)((unsafe.Pointer)(&in[0]))
   fftw_out := (*C.fftw_complex)((unsafe.Pointer)(&out[0]))
-  p := C.fftw_plan_dft_1d((C.int)(len(in)), fftw_in, fftw_out, C.int(dir), C.FFTW_ESTIMATE)
+  p := C.fftw_plan_dft_1d((C.int)(len(in)), fftw_in, fftw_out, C.int(dir), C.uint(flag))
   return newPlan(p)
 }
 
-func PlanDft2d(in,out [][]complex128, dir Direction) *plan {
+func PlanDft2d(in,out [][]complex128, dir Direction, flag Flag) *plan {
   // TODO: check that in and out have the same dimensions
   fftw_in := (*C.fftw_complex)((unsafe.Pointer)(&in[0][0]))
   fftw_out := (*C.fftw_complex)((unsafe.Pointer)(&out[0][0]))
-  p := C.fftw_plan_dft_2d((C.int)(len(in)), (C.int)(len(in[0])), fftw_in, fftw_out, C.int(dir), C.FFTW_ESTIMATE)
+  p := C.fftw_plan_dft_2d((C.int)(len(in)), (C.int)(len(in[0])), fftw_in, fftw_out, C.int(dir), C.uint(flag))
   return newPlan(p)
 }
 
