697afdd48c46816e10354f562bfde339aca46001
Jonathan Wills <runningwild@gmail.com>
2011-03-19 18:54:11 -0400

    All tests are run now, some in serial.

--- all_specs_test.go
+++ all_specs_test.go
@@ -11,8 +11,17 @@ func TestAllSpecs(t *testing.T) {
   r.AddSpec(Alloc1dSpec)
   r.AddSpec(Alloc2dSpec)
   r.AddSpec(Alloc3dSpec)
-//  r.AddSpec(FFT1dSpec)
-//  r.AddSpec(FFT2dSpec)
+  gospec.MainGoTest(r, t)
+  
+  // TODO: Investigate a less stupid way of doing tests in serial
+  // TODO: Investigate what about fftw makes tests need to be serial
+  r = gospec.NewRunner()
+  r.AddSpec(FFT1dSpec)
+  gospec.MainGoTest(r, t)
+  r = gospec.NewRunner()
+  r.AddSpec(FFT2dSpec)
+  gospec.MainGoTest(r, t)
+  r = gospec.NewRunner()
   r.AddSpec(FFT3dSpec)
   gospec.MainGoTest(r, t)
 }
